5934996362a17a898fd6cea20e19ad276bb97e11
AsyrafMufadhdhal <muhammad.asyraf.ma13@gmail.com>
2024-03-09 10:06:42 +0700

    add code to auto refresh page in midnight

--- src/component/Clock/index.jsx
+++ src/component/Clock/index.jsx
@@ -4,8 +4,15 @@ const Clock = () => {
   let currentTime = new Date().toLocaleTimeString("en-US", { hour12: false });
   const [time, setTime] = useState(currentTime);
 
+  // Code for realtime clock
   useEffect(() => {
     const updateTime = setInterval(() => setTime(new Date().toLocaleTimeString("en-US", { hour12: false })), 1000);
+
+    //in midnight refresh page to load new data
+    if (time == "00:00:00" || time == "24:00:00") {
+      console.log("halaman di reload");
+      window.location.reload();
+    }
     return () => clearInterval(updateTime);
   }, [time]);
   return <>{time}</>;
