53f9aeeac0ac1bcac8c16adbe8b198bb635ce793
AsyrafMufadhdhal <muhammad.asyraf.ma13@gmail.com>
2024-03-09 10:17:11 +0700

    change bootstrap class

--- src/component/DateTime/index.jsx
+++ src/component/DateTime/index.jsx
@@ -15,12 +15,12 @@ const DateTime = ({ date }) => {
     <div className="row px-5">
       <div className="col-auto fs-5 flex-grow-1 align-self-center fw-semibold text-start text-white">{formatDate(date)}</div>
 
-      <div className={`col-auto rounded-start-pill px-4 d-flex align-items-center ${isFinished && "d-none"} `} id={styles.nextName}>
+      <div className={`col-auto rounded-start-pill px-5 d-flex align-items-center ${isFinished && "d-none"} `} id={styles.nextName}>
         <p className="fw-semibold m-0 rounded-pill ">
           Menuju Waktu <span id={styles.nextTitle}> {now == "terbit" ? (now = "syuruq") : now}</span>
         </p>
       </div>
-      <div className={`col-3 text-center p-0 rounded-end-pill ${isFinished && "d-none"}`} id={styles.nextTime}>
+      <div className={`col-2 text-center p-0 rounded-end-pill ${isFinished && "d-none"}`} id={styles.nextTime}>
         <p className="fw-bold m-0 rounded-pill" id={styles.nextTimeContent}>
           <Countdown />
         </p>
--- src/component/TimePin/index.jsx
+++ src/component/TimePin/index.jsx
@@ -2,11 +2,12 @@ import styles from "../../component/TimePin/TimePin.module.css";
 
 const TimePin = ({ next, prayerTimes }) => {
   return (
-    <div className="row px-5 d-flex justify-content-evenly column-gap-5 row-gap-5">
+    // do mapping to data from API to display all the prayer time of the day
+    <div className="row row-cols-3 row-cols-lg-auto d-flex justify-content-evenly gap-5 px-5">
       {prayerTimes.map((prayerTime, index) => {
         let title = String(Object.keys(prayerTime));
         return (
-          <div className="col-auto p-0" key={index}>
+          <div className="col p-0" key={index}>
             <div className={`d-flex rounded-circle ${title == next ? styles.next : ""}`} id={styles.timePin}>
               <div className="align-self-center text-center mx-auto">
                 <h2 className="mb-2" id={styles.title}>
